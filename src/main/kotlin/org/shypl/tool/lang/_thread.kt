0402a05bfb7fd39c60847152818f62185dd213c4
Alexey Chuprin <a.chuprin@capjack.ru>
2026-03-05 01:21:40 +0300

    Improvements

--- src/main/kotlin/org/shypl/tool/lang/_thread.kt
+++ src/main/kotlin/org/shypl/tool/lang/_thread.kt
@@ -1,27 +1,29 @@
 package org.shypl.tool.lang
 
-inline fun waitUntil(maxTimeoutMillis: Int, checkTimeoutMillis: Int = 10, condition: () -> Boolean) =
-	condition().letOnFalse { waitUntilLater(maxTimeoutMillis, checkTimeoutMillis, condition) }
+inline fun waitUntil(maxTimeoutMillis: Long, checkTimeoutMillis: Long = 10, condition: () -> Boolean): Boolean {
+	return condition().letOnFalse { waitUntilLater(maxTimeoutMillis, checkTimeoutMillis, condition) }
+}
 
-inline fun waitWhile(maxTimeoutMillis: Int, checkTimeoutMillis: Int = 10, condition: () -> Boolean) =
-	condition().letOnTrue { waitWhileLater(maxTimeoutMillis, checkTimeoutMillis, condition) }
+inline fun waitWhile(maxTimeoutMillis: Long, checkTimeoutMillis: Long = 10, condition: () -> Boolean): Boolean {
+	return condition().letOnTrue { waitWhileLater(maxTimeoutMillis, checkTimeoutMillis, condition) }
+}
 
-inline fun waitUntilLater(maxTimeoutMillis: Int, checkTimeoutMillis: Int = 10, condition: () -> Boolean) =
-	waitFor(true, maxTimeoutMillis, checkTimeoutMillis, condition)
+inline fun waitUntilLater(maxTimeoutMillis: Long, checkTimeoutMillis: Long = 10, condition: () -> Boolean): Boolean {
+	return waitFor(true, maxTimeoutMillis, checkTimeoutMillis, condition)
+}
 
-inline fun waitWhileLater(maxTimeoutMillis: Int, checkTimeoutMillis: Int = 10, condition: () -> Boolean) =
-	waitFor(false, maxTimeoutMillis, checkTimeoutMillis, condition)
-
+inline fun waitWhileLater(maxTimeoutMillis: Long, checkTimeoutMillis: Long = 10, condition: () -> Boolean): Boolean {
+	return waitFor(false, maxTimeoutMillis, checkTimeoutMillis, condition)
+}
 
 @PublishedApi
-internal inline fun waitFor(expected: Boolean, maxTimeoutMillis: Int, checkTimeoutMillis: Int = 10, condition: () -> Boolean): Boolean {
+internal inline fun waitFor(expected: Boolean, maxTimeoutMillis: Long, checkTimeoutMillis: Long, condition: () -> Boolean): Boolean {
 	var result: Boolean
-	val checkTime = checkTimeoutMillis.toLong()
-	val maxTime = maxTimeoutMillis * 1_000_000L
+	val maxTime = maxTimeoutMillis * 1_000_000
 	val startTime = System.nanoTime()
 	do {
 		try {
-			Thread.sleep(checkTime)
+			Thread.sleep(checkTimeoutMillis)
 		}
 		catch (_: InterruptedException) {
 			Thread.currentThread().interrupt()
